8c8eef0e4cc6dd10c8c065d6cf6c448dc141546c
raam <raam.g@gmail.com>
2022-05-01 22:17:31 +0530

    sec2n comit

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -5,7 +5,7 @@ namespace WebApplicationdemo
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        //hello chck 
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
